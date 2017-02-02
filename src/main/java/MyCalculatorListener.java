423215ac95a75c80dbcc8a004be1ead574ec4eba
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2017-02-02 13:13:05 +0000

    fix capitalisation to make the code work better on windows

--- src/main/java/MyCalculatorListener.java
+++ src/main/java/MyCalculatorListener.java
@@ -3,15 +3,15 @@ import org.antlr.v4.runtime.misc.NotNull;
 /**
  * Created by webpigeon on 25/01/17.
  */
-public class CalculatorListener extends calculatorBaseListener {
+public class MyCalculatorListener extends CalculatorBaseListener {
 
     @Override
-    public void enterNumber(@NotNull calculatorParser.NumberContext ctx) {
+    public void enterNumber(@NotNull CalculatorParser.NumberContext ctx) {
         System.out.println("found number: "+ctx.getText());
     }
 
     @Override
-    public void enterExpression(@NotNull calculatorParser.ExpressionContext ctx) {
+    public void enterExpression(@NotNull CalculatorParser.ExpressionContext ctx) {
         System.out.println("found expression: "+ctx.getText()+" depth: "+ctx.depth());
     }
 }
